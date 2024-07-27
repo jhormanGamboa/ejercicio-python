96356dd9a462126168648f7f8706306baf441f94
jesusdavid1234 <jesusflorez20.david@gmail.com>
2024-07-27 15:53:21 -0500

    Update controlador_usuario.py

--- main/controlador_usuario.py
+++ main/controlador_usuario.py
@@ -1,2 +1,37 @@
+from modelo import modelo_e
+from vista_formulario import vistaformulario
 class controlador_formulario:
+    def __init__(self,obmodelo,objvista) :
+        self.objmodelo=obmodelo
+        self.obvista=objvista
+
+    def enviardatos(self):
+        nombre = self.obvista.entry_nombre.get()
+        apellido = self.obvista.entry_apellido.get()
+        edad = self.obvista.entry_edad.get()
+        correo = self.obvista.entry_correo.get()
+        genero = self.obvista.entry_genero.get()
+
+        datos = {
+            "nombre": nombre,
+            "apellido": apellido,
+            "edad": edad,
+            "correo": correo,
+            "genero": genero
+        }
+
+        self.objmodelo.escribir_json("datos.json", datos)
+        self.obvista.lab_resultado.config(text="Datos enviados correctamente.")
+
+
+obmodelo=modelo_e
+obvista=vistaformulario()
+obcontrolador=controlador_formulario(obmodelo,obvista)
+obcontrolador.enviardatos()
+obvista.crear_boton()
+
+
+obvista.iniciar()
+
+
     
